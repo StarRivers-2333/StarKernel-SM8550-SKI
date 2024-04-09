5f24ba9a8c711d3b45ef1ecab0c70333e25cb39c
maoshenshen <maoshenshen@oppo.com>
2024-04-09 20:34:38 +0800

    ANDROID: vendor_hooks: add hooks in cpu_cgroup subsystem
    Add hooks to tune cpu policy based on cpu_cgroup.
    
    Bug: 333664691
    Bug: 438711811
    
    Change-Id: If7ca0f2ae774f433563abfbe4e761f3fb9645633
    Signed-off-by: maoshenshen <maoshenshen@oppo.com>
    (cherry picked from commit 66b74bb79ddfe12ea63b05880eef0548aa4bf6d8)
    Signed-off-by: pugaowei <pugaowei@oppo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -31,10 +31,6 @@
 #include <trace/hooks/dtask.h>
 #include <trace/hooks/cgroup.h>
 
-#if IS_ENABLED(CONFIG_OPLUS_SCHED_TUNE)
-#include <../kernel/oplus_cpu/sched/sched_tune/tune.h>
-#endif
-
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
@@ -100,8 +96,6 @@ const_debug unsigned int sysctl_sched_nr_migrate = 32;
  * default: 1s
  */
 unsigned int sysctl_sched_rt_period = 1000000;
-
-unsigned long stop_fair_group = 0;
 
 __read_mostly int scheduler_running;
 
@@ -10159,11 +10153,7 @@ void sched_move_task(struct task_struct *tsk)
 	struct rq_flags rf;
 	struct rq *rq;
 
-#if IS_ENABLED(CONFIG_OPLUS_SCHED_TUNE)
-	schedtune_attach(tsk);
-#endif
 	trace_android_vh_sched_move_task(tsk);
-
 	rq = task_rq_lock(tsk, &rf);
 	update_rq_clock(rq);
 
@@ -10204,9 +10194,7 @@ cpu_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 	struct task_group *tg;
 
 	if (!parent) {
-#if IS_ENABLED(CONFIG_OPLUS_SCHED_TUNE)
-		schedtune_root_alloc();
-#endif
+		trace_android_vh_cpu_cgroup_css_alloc_early(parent);
 		/* This is early initialization for the top cgroup */
 		return &root_task_group.css;
 	}
@@ -10214,9 +10202,9 @@ cpu_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 	tg = sched_create_group(parent);
 	if (IS_ERR(tg))
 		return ERR_PTR(-ENOMEM);
-#if IS_ENABLED(CONFIG_OPLUS_SCHED_TUNE)
-	schedtune_alloc(tg, parent_css);
-#endif
+
+	trace_android_vh_cpu_cgroup_css_alloc(tg, parent_css);
+
 	return &tg->css;
 }
 
@@ -10257,9 +10245,8 @@ static void cpu_cgroup_css_free(struct cgroup_subsys_state *css)
 	 * Relies on the RCU grace period between css_released() and this.
 	 */
 	sched_unregister_group(tg);
-#if IS_ENABLED(CONFIG_OPLUS_SCHED_TUNE)
-	schedtune_free(css);
-#endif
+
+	trace_android_vh_cpu_cgroup_css_free(css);
 }
 
 /*
@@ -10620,14 +10607,6 @@ static u64 cpu_shares_read_u64(struct cgroup_subsys_state *css,
 			       struct cftype *cft)
 {
 	struct task_group *tg = css_tg(css);
-
-	/*
-	 * We can't change the weight of the root cgroup.
-	 */
-	if (!tg->se[0])
-	{
-		return (u64)stop_fair_group;
-	}
 
 	return (u64) scale_load_down(tg->shares);
 }
@@ -11062,13 +11041,6 @@ static struct cftype cpu_legacy_files[] = {
 		.write_u64 = cpu_uclamp_ls_write_u64,
 	},
 #endif
-#if IS_ENABLED(CONFIG_OPLUS_SCHED_TUNE)
-	{
-		.name = "schedtune.boost",
-		.read_s64 = schedtune_boost_read,
-		.write_s64 = schedtune_boost_write,
-	},
-#endif
 	{ }	/* Terminate */
 };
 
@@ -11267,14 +11239,6 @@ static struct cftype cpu_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_uclamp_ls_read_u64,
 		.write_u64 = cpu_uclamp_ls_write_u64,
-	},
-#endif
-#if IS_ENABLED(CONFIG_OPLUS_SCHED_TUNE)
-	{
-		.name = "schedtune.boost",
-		.flags = CFTYPE_NOT_ON_ROOT,
-		.read_s64 = schedtune_boost_read,
-		.write_s64 = schedtune_boost_write,
 	},
 #endif
 	{ }	/* terminate */
